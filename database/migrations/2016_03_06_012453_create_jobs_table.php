973f66d8c16a3a3e1c9163551441ed5f406edc17
David Alves de Souza <alves.david@outlook.com>
2016-03-06 10:19:54 -0300

    Criação das migrations

---
+++ database/migrations/2016_03_06_012453_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->datetime('datetime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jobs');
+    }
+}
