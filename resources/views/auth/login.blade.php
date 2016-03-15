d9cbad9fc41c6378d583aa4372a6ac5c33b12acb
David Alves de Souza <alves.david@outlook.com>
2016-03-14 22:58:26 -0300

    Atualização do layout de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Agenda InApp</title>
+@extends('layouts.log')
 
-    <link rel="stylesheet" href="{{asset('assets/css/style.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/bower/components-font-awesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/bower/bootstrap/dist/css/bootstrap.min.css')}}">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="login panel panel-default">
-                <h2 class="text-center">Agenda InApp</h2>
-                <hr>
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                    {!! csrf_field() !!}
+@section('content')
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <input type="email" class="form-control" name="email" placeholder="E-mail" value="{{ old('email') }}" ng-required>
+<div class="row">
+    <div class="login">
+        <h2 class="text-center">Agenda InApp</h2>
+        <hr>
+        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+            {!! csrf_field() !!}
 
-                        @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                        @endif
-                    </div>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <input type="email" class="form-control" name="email" placeholder="E-mail" value="{{ old('email') }}" ng-required>
 
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <input type="password" class="form-control" name="password" placeholder="Senha" ng-required>
-                        @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-btn fa-sign-in"></i>Login
-                            </button>
-
-                            <a class="btn btn-link" href="{{ url('/password/reset') }}">Esqueceu a senha?</a>
-                        </div>
-                    </div>
-                </form>
+                @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
             </div>
-        </div>
-    </div>
 
-    <script src="{{asset('assets/bower/jquery/dist/jquery.min.js')}}"></script>
-    <script src="{{asset('assets/bower/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-</body>
-</html>
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <input type="password" class="form-control" name="password" placeholder="Senha" ng-required>
+                @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-btn fa-sign-in"></i>Login
+                    </button>
+
+                    <a class="btn btn-link" href="{{ url('/password/reset') }}">Esqueceu a senha?</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+@stop
--- resources/views/login.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-
-@section('content')
-
-	<div class="jumbotron text-center">
-		<h2 class="text-center"> Lumen.</h2>
-	</div>
-
-@stop
